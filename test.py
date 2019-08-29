aa43a5ccccdc088a6202a6a1e887b4e3e893ad41
pravinahire <pravinahired@gmail.com>
2019-08-29 12:25:47 +0530

    Add files via upload

---
+++ test.py
@@ -0,0 +1,7 @@
+s = input()
+l = s.split(',') 
+for i in l:
+	int(i)
+
+m=int(l[1])*2
+print(m)
---
+++ test1.py
@@ -0,0 +1,6 @@
+list =[]
+number = int(input("how many value you want in a list: "))
+for i in range(0,number): 
+	numbers = int(input("enter your choice number:"))
+	list. append(numbers)
+print(list)
